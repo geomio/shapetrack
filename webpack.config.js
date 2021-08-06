20dd2d62d2bed91ab447e1baddadea008370db1e
Geoffry Millner <geoff.millner22@gmail.com>
2021-08-06 15:07:57 -0700

    fixed error on line 13 on webpack config added .

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,28 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   entry: './src/main.js',
   output: {
     filename: 'bundle.js',
-    path: path.resolve(_direname, 'dist')
+    path: path.resolve(__dirname, 'dist')
+  },
+  plugins: [
+    new HtmlWebpackPlugin ({
+      title: 'Shaper Tracker',
+      template: './src/index.html',
+      inject: 'body'
+    })
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use:[
+          'style-loader',
+          'css-loader'
+        ]
+      }
+    ]
   }
 };
